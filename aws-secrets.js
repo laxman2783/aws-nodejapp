5d52219f6acb35f047bc374857ec7e567b577a94
laxman2783 <laxmant.thota@gmail.com>
2023-05-23 00:27:43 +0530

    Add files via upload

--- aws-secrets.js
+++ aws-secrets.js
@@ -2,10 +2,10 @@
 // If you need more information about configurations or implementing the sample code, visit the AWS docs:
 // https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
 
-import {
+const {
     SecretsManagerClient,
     GetSecretValueCommand,
-  } from "@aws-sdk/client-secrets-manager";
+  } = require("@aws-sdk/client-secrets-manager");
   
   const secret_name = "test-secret-manager";
   
