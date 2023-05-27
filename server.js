c3c2364b70904b744486517a56b3f8cb2b0c7018
laxman2783 <laxmant.thota@gmail.com>
2023-05-27 16:53:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/',(req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World'));
+
+
+app.get("/getSecrets", (req, res) => {
+    res.send("This is secret");
+    fetch("https://mt3itmqqbyedboiy6ftx4rsjcu0zqcwj.lambda-url.us-east-2.on.aws/")
+        .then(res => res.json())
+        .then(json => console.log(json));
+        req.end();
+})
 
 app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
+
+// function to connect to  graphql calls
+
+
+
+
+
